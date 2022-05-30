6617d17a7391df71ca838735463f18355e1fc85d
Stanislav Bosievskoy <stanislav.bosievskoy@nitrix.io>
2022-05-30 16:55:46 +0300

    master

--- js/ex.js
+++ js/ex.js
@@ -1,10 +1,27 @@
-var spotlight = document.querySelector('.spotlight');
-var spotlightSize = 'transparent 300px, rgba(0, 0, 0, 0.5) 200px)';
+// var spotlight = document.querySelector('.spotlight');
+// var spotlightSize = 'transparent 300px, rgba(0, 0, 0, 0.5) 200px)';
         
-window.addEventListener("DOMContentLoaded", () => {
-  window.addEventListener('mousemove', e => updateSpotlight(e));
+// window.addEventListener("DOMContentLoaded", () => {
+//   window.addEventListener('mousemove', e => updateSpotlight(e));
 
-  function updateSpotlight(e) {
-    spotlight.style.backgroundImage = `url(../img/main-screen-background.png)(circle at ${e.pageX / window.innerWidth * 100}% ${e.pageY / window.innerHeight * 100}%, ${spotlightSize}`;
+//   function updateSpotlight(e) {
+//     spotlight.style.backgroundImage = `url(../img/main-screen-background.png)(circle at ${e.pageX / window.innerWidth * 100}% ${e.pageY / window.innerHeight * 100}%, ${spotlightSize}`;
+//     }
+// });
+
+document.getElementById("horizontal-scroller")
+  .addEventListener('wheel', function(event) {
+    if (event.deltaMode == event.DOM_DELTA_PIXEL) {
+      var modifier = 1;
+      // иные режимы возможны в Firefox
+    } else if (event.deltaMode == event.DOM_DELTA_LINE) {
+      var modifier = parseInt(getComputedStyle(this).lineHeight);
+    } else if (event.deltaMode == event.DOM_DELTA_PAGE) {
+      var modifier = this.clientHeight;
     }
-});
+    if (event.deltaY != 0) {
+      // замена вертикальной прокрутки горизонтальной
+      this.scrollLeft += modifier * event.deltaY;
+      event.preventDefault();
+    }
+  });
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-var navMain = document.querySelector('.nav-list');
-var navToggle = document.querySelector('.nav__toggle');
+let navMain = document.querySelector('.nav-list');
+let navToggle = document.querySelector('.nav__toggle');
 
 
 navToggle.addEventListener('click', function() {
@@ -9,5 +9,63 @@ navToggle.addEventListener('click', function() {
   } else {
     navMain.classList.add('nav-closed');
     navMain.classList.remove('nav-opened');
+  }
+});
+
+
+
+let ddtList = document.querySelector('.ddt-faq-inner');
+let ddtToggle = document.querySelector('.ddt-faq-subtitle');
+
+
+ddtToggle.addEventListener('click', function() {
+  if (ddtList.classList.contains('faq-closed')) {
+    ddtList.classList.remove('faq-closed');
+    ddtList.classList.add('faq-opened');
+  } else {
+    ddtList.classList.add('faq-closed');
+    ddtList.classList.remove('faq-opened');
+  }
+});
+
+let ddtListOne = document.querySelector('.ddt-faq-inner1');
+let ddtToggleOne = document.querySelector('.ddt-faq-subtitle1');
+
+
+ddtToggleOne.addEventListener('click', function() {
+  if (ddtListOne.classList.contains('faq-closed1')) {
+    ddtListOne.classList.remove('faq-closed1');
+    ddtListOne.classList.add('faq-opened1');
+  } else {
+    ddtListOne.classList.add('faq-closed1');
+    ddtListOne.classList.remove('faq-opened1');
+  }
+});
+
+let ddtListTwo = document.querySelector('.ddt-faq-inner2');
+let ddtToggleTwo = document.querySelector('.ddt-faq-subtitle2');
+
+
+ddtToggleTwo.addEventListener('click', function() {
+  if (ddtListTwo.classList.contains('faq-closed2')) {
+    ddtListTwo.classList.remove('faq-closed2');
+    ddtListTwo.classList.add('faq-opened2');
+  } else {
+    ddtListTwo.classList.add('faq-closed2');
+    ddtListTwo.classList.remove('faq-opened2');
+  }
+});
+
+let ddtListThree = document.querySelector('.ddt-faq-inner3');
+let ddtToggleThree = document.querySelector('.ddt-faq-subtitle3');
+
+
+ddtToggleThree.addEventListener('click', function() {
+  if (ddtListThree.classList.contains('faq-closed3')) {
+    ddtListThree.classList.remove('faq-closed3');
+    ddtListThree.classList.add('faq-opened3');
+  } else {
+    ddtListThree.classList.add('faq-closed3');
+    ddtListThree.classList.remove('faq-opened3');
   }
 });
